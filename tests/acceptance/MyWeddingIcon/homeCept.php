b055396a3d06b81135739dcfbedbe157df9fd304
Himanshu Suri <himanshu@aroracomfortechs.com>
2017-05-03 16:47:07 +0530

    login changes as per new layout

--- tests/acceptance/MyWeddingIcon/homeCept.php
+++ tests/acceptance/MyWeddingIcon/homeCept.php
@@ -3,11 +3,10 @@
 	$I->wantTo('perform actions and see result');
 	$I->amOnUrl('http://dev.myweddingicon.com/');
 	$I->comment('LOGIN A USER');	
-	$I->click('#signin');	
-	
+	$I->click('#signin');		
 	$I->fillField('#create_account_email','admin@gmail.com');
-	$I->fillField('//div[contains(@class,"b-form-row")] //div[contains(@class,"b-form-horizontal__input")] //input[contains(@class,"form-control")] ','qwerty');
-	$I->checkOption('//div[contains(@class,"b-form-row")] //label[contains(@for,"contact_form_copy")] //input[contains(@name,"remember")]');
+	$I->fillField('//div[contains(@class,"col-sm-12")] //span[contains(@class,"input")] //input[contains(@class,"input_field")] ','qwerty');
+	$I->checkOption('//div[contains(@class,"b-form-row col-sm-12")] //label[contains(@for,"contact_form_copy")] //input[contains(@name,"remember")]');
 	$I->click('#btn_submit');
 	
 
